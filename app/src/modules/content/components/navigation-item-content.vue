198435df9de99afd208fc88ef1d10555865c5a50
Nicolas Thomas <thedeadofblackfire@yahoo.com>
2023-05-22 15:55:13 +0200

    [WIP] change naming for content navigation

--- app/src/modules/content/components/navigation-item-content.vue
+++ app/src/modules/content/components/navigation-item-content.vue
@@ -2,11 +2,13 @@
 	<v-list-item-icon>
 		<v-icon :name="icon ?? 'label'" :color="color" />
 	</v-list-item-icon>
-	<v-list-item-content><v-text-overflow :text="name" :highlight="search" /></v-list-item-content>
+	<v-list-item-content><v-text-overflow :text="naming" :highlight="search" /></v-list-item-content>
 </template>
 
 <script setup lang="ts">
-withDefaults(
+import { computed } from 'vue';
+
+const props = withDefaults(
 	defineProps<{
 		name: string;
 		search?: string;
@@ -18,4 +20,9 @@ withDefaults(
 		color: 'var(--foreground-normal)',
 	}
 );
+
+const naming = computed(() => {
+	if (props.name.indexOf('Commerce ') === 0) return props.name.replace('Commerce ', '');
+	else return props.name;
+});
 </script>
